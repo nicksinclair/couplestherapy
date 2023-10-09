5f7093b5dac099ed352d239a34a9c7f6919ff9a1
Nick Sinclair <nick@hypar.io>
2023-10-09 15:11:14 -0400

    Install prettier, add music page, embed SoundCloud

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.setBrowserSyncConfig({
-        files: "./public/styles/**/*.css",
-    });
+  eleventyConfig.setBrowserSyncConfig({
+    files: './public/styles/**/*.css',
+  });
 
-    eleventyConfig.addPassthroughCopy("./src/styles");
+  eleventyConfig.addPassthroughCopy('./src/styles');
 
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
-    eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true);
 
-    return {
-        passthroughFileCopy: true,
-        dir: {
-            input: "src",
-            output: "public",
-        },
-    };
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+  };
 };
