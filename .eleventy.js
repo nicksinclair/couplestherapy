bd8b042e71c498c5978b3a8d5b1acbc93713540d
Nick Sinclair <nick@hypar.io>
2023-10-13 01:10:54 -0400

    Support images, add shows, style navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,25 @@
+const pluginImages = require('./.eleventy.config.images.js');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     files: './public/styles/**/*.css',
   });
 
+  // eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addPassthroughCopy('./src/styles');
 
+  // App plugins
+  eleventyConfig.addPlugin(pluginImages);
+
+  // Shortcodes
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   eleventyConfig.setDataDeepMerge(true);
 
   return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     passthroughFileCopy: true,
     dir: {
       input: 'src',
--- src/pages/music.md
+++ src/pages/music.md
@@ -2,6 +2,8 @@
 title: 'Music'
 ---
 
+# Music
+
 <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1690750659&color=%23a0a0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 
 <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
